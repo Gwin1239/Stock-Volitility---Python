0014d241661cb6722d2b0dee36bba786746a8bc7
Godwin Djossou <90664989+Gwin1239@users.noreply.github.com>
2022-01-17 12:31:59 -0600

    Add files via upload

---
+++ stocktracker.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+api_key = 'FEMZVFSLI14MYYOC'
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='TSLA', interval='1min', outputsize= 'full')
+print(data)
+
+i = 1
+# while i == 1:
+#    data, meta_data = ts.get_intraday(symbol='TSLA', interval='1min', outputsize='full')
+#   data.to_excel("output.xlsx")
+#    time.sleep(60)
+
+close_data = data['4. close']
+percentage_change = close_data.pct_change()
+
+print(percentage_change)
+
+last_change = percentage_change[-1]
+
+if abs(last_change) > 0.005:
+    print("TSLA Alert:" + last_change)
